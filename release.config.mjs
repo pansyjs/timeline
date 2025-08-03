c1b36e4f7d0745067dd1d9355786936b2d3de623
wangxingkang <wang_xingkang@qq.com>
2025-08-03 23:12:55 +0800

    chore: update release config

--- release.config.mjs
+++ release.config.mjs
@@ -20,7 +20,7 @@ export default {
         assets: ['CHANGELOG.md', 'package.json'],
         message:
           // eslint-disable-next-line no-template-curly-in-string
-          'chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
+          'chore(release): ${nextRelease.gitTag} [skip ci]',
       },
     ],
   ],
